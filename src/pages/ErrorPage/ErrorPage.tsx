d3171113c6223db1ed6b9f57e6be38c3f305bfaa
DIANA IOSIF <alex_diana3198@yahoo.com>
2023-03-12 13:20:34 +0200

    added sass module + implemented error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,21 @@
+import noResultsImage from "../../assests/no-results-illustration-a.svg";
+
+import styles from "./errorPage.module.scss";
+
 const ErrorPage = () => {
-  return <div>Error Page</div>;
+  return (
+      <div className={styles.noResultsContainer}>
+        <div className={styles.innerContainer}>
+          <img
+            className={styles.noResultsTextMessage}
+            src={noResultsImage}
+            height={300}
+            alt=""
+          />
+          <h2 className={styles.noResultsTextMessage}>This page is not found</h2>
+        </div>
+      </div>
+  );
 };
 
 export default ErrorPage;
