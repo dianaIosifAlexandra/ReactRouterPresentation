c0833c7904ac9b5ce92ea6734269d7e3f3f35285
DIANA IOSIF <alex_diana3198@yahoo.com>
2023-03-15 16:54:30 +0200

    create custom components

--- src/components/Drawer/ResponsiveDrawer.tsx
+++ src/components/Drawer/ResponsiveDrawer.tsx
@@ -3,65 +3,54 @@ import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import "./responsive-drawer.styles.scss";
 import { Typography } from "@mui/material";
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
 
 interface Props {
   children?: React.ReactNode;
+  open?: boolean;
 }
 
 export default function ResponsiveDrawer({ children }: Props) {
   const [open, setOpen] = useState(false);
-  const theme = useTheme();
-  //   const navigate = useNavigate();
 
-  const drawerWidth = {
-    width: "50px",
-    "@media (max-width: 700px)": {
-      width: "100%",
-    },
+  const drawerWidth = 50;
+
+  const Main = ({ children, open }: Props) => {
+    return (
+      <Box
+        style={open ? { marginLeft: 0 } : { marginLeft: "-50px" }}
+        className="main-container"
+      >
+        {children}
+      </Box>
+    );
   };
 
-  const Main = styled("main", {
-    shouldForwardProp: (prop) => prop !== "open",
-  })<{
-    open?: boolean;
-  }>(({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    marginLeft: `-50px`,
-    ...(open && {
-      marginLeft: 0,
-    }),
-  }));
+  const AppBar = ({ children, open }: Props) => {
+    return (
+      <MuiAppBar
+        position="fixed"
+        style={{
+          width: open ? `calc(100% - ${drawerWidth})` : "100%",
+          marginLeft: open ? `${drawerWidth}` : 0,
+        }}
+        className="app-bar"
+      >
+        {children}
+      </MuiAppBar>
+    );
+  };
 
-  const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== "open",
-  })<AppBarProps>(({ theme, open }) => ({
-    ...(open && {
-      width: `calc(100% - ${drawerWidth})`,
-      marginLeft: `${drawerWidth}`,
-    }),
-  }));
-
-  const DrawerHeader = styled("div")(({ theme }) => ({
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
-  }));
+  const DrawerHeader = ({ children }: Props) => {
+    return <div className="drawer-header">{children}</div>;
+  };
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -74,14 +63,7 @@ export default function ResponsiveDrawer({ children }: Props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        style={{
-          backgroundColor: "white",
-          boxShadow: "0px 4px 4px 2px #f5f5f5",
-        }}
-        position="fixed"
-        open={open}
-      >
+      <AppBar open={open}>
         <Toolbar className="toolbar">
           <IconButton
             color="inherit"
@@ -118,17 +100,13 @@ export default function ResponsiveDrawer({ children }: Props) {
         <DrawerHeader>
           <div className="drawer-header-container">
             <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "ltr" ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
+              <ChevronLeftIcon />
             </IconButton>
           </div>
         </DrawerHeader>
         <Divider />
       </Drawer>
-      <Main className="main-container" open={open}>
+      <Main open={open}>
         <div className="content-container">{children}</div>
       </Main>
     </Box>
